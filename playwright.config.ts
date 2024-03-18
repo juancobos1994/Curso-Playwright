9759f6907b936a548e6362009ca3d57d160e10a3
Juan Cobos <juancho_cobos@hotmail.com>
2024-03-18 15:16:11 -0500

    Carpeta Github2

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    headless: true,
     launchOptions: {
       slowMo: 1000
     },
